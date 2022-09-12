10513d2974857c95e4e94f5af43c8d250eae73cf
nicola giannotta <nicolagiannotta84@gmail.com>
2022-09-12 16:21:53 +0100

    validate props for the forecast summaries complete

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,29 @@ import React from "react";
 import "../styles/App.css";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 
-function App({ location }) {
-  const { city, country } = location;
-  return <LocationDetails city={city} country={country} />;
+function App({ location, forecasts }) {
+  return (
+    <div>
+      <LocationDetails city={location.city} country={location.country} />
+      <ForecastSummaries forecasts={forecasts} />
+    </div>
+  );
 }
 App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
+
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
