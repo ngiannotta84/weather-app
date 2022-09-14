1c808a3fb60ecebad4a2195f12bde18e5d856df0
nicola giannotta <nicolagiannotta84@gmail.com>
2022-09-14 19:56:38 +0100

    npm weather icon installed and summary and Forecast summaries tests updated

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import WeatherIcon from "react-icons-weather";
 
 function ForecastSummary(props) {
   const { date, temperature, description, icon } = props;
@@ -7,7 +8,7 @@ function ForecastSummary(props) {
     <div className="forecast-summary" data-testid="forecast-summary">
       <div className="forecast-summary__date">{date}</div>
       <div className="forecast-summary__icon" data-testid="forecast-icon">
-        {icon}
+        <WeatherIcon name="owm" iconId={icon} />
       </div>
       <div className="forecast-summary__temperature">
         {temperature.max}
